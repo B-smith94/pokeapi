43df32cc3dd491cf09b5c3a638a86bca561d1a07
Byron Smith <byrondsmith9@gmail.com>
2024-12-04 11:03:18 -0500

    set up search for pokemon by name

---
+++ pokeapi.js
@@ -0,0 +1,27 @@
+async function fetchPokemonData(pokemonName) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    const pokemonData = await response.json();
+    return pokemonData;
+}
+
+document.addEventListener("input", async () => {
+    let pokeName= document.getElementById('search-name').value.toLowerCase();
+    const pokemonData = await fetchPokemonData(pokeName);
+    const pokemonInfoElement = document.getElementById('pokemon-info');
+    
+    pokemonInfoElement.innerHTML = `
+    <h2>${pokemonData.name}</h2>
+    <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
+    <h3>Types:</h3>
+    <ul>
+        ${pokemonData.types.map(type => `<li>${type.type.name}</li>`).join('')}
+    <h3>Abilities:</h3>
+    <ul>
+        ${pokemonData.abilities.map(ability => `<li>${ability.ability.name}</li>`).join('')}
+    </ul>
+    <h3>Stats:</h3>
+    <ul>
+        ${pokemonData.stats.map(stat => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
+    `;
+});
+
