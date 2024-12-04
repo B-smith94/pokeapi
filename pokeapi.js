af4eeae165d5d464cebd2496a0ef4aba1b41e63d
Byron Smith <byrondsmith9@gmail.com>
2024-12-04 15:34:18 -0500

    added styling to the form and info display

--- pokeapi.js
+++ pokeapi.js
@@ -10,18 +10,21 @@ document.addEventListener("input", async () => {
     const pokemonInfoElement = document.getElementById('pokemon-info');
     
     pokemonInfoElement.innerHTML = `
-    <h2>${pokemonData.name}</h2>
-    <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
-    <h3>Types:</h3>
-    <ul>
-        ${pokemonData.types.map(type => `<li>${type.type.name}</li>`).join('')}
-    <h3>Abilities:</h3>
-    <ul>
-        ${pokemonData.abilities.map(ability => `<li>${ability.ability.name}</li>`).join('')}
-    </ul>
-    <h3>Stats:</h3>
-    <ul>
-        ${pokemonData.stats.map(stat => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
-    `;
+    <div class="m-2">
+        <h2>Name: ${pokemonData.name}</h2>
+        <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
+        <h3>Types:</h3>
+        <ul>
+            ${pokemonData.types.map(type => `<li>${type.type.name}</li>`).join('')}
+        </ul>
+        <h3>Abilities:</h3>
+        <ul>
+            ${pokemonData.abilities.map(ability => `<li>${ability.ability.name}</li>`).join('')}
+        </ul>
+        <h3>Stats:</h3>
+        <ul>
+            ${pokemonData.stats.map(stat => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
+        </ul>
+    </div>`;
 });
 
