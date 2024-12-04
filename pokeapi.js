21e6dc9ac3ed438e374a1485c7daabfe68934b20
Byron Smith <byrondsmith9@gmail.com>
2024-12-04 16:01:50 -0500

    added comments

--- pokeapi.js
+++ pokeapi.js
@@ -1,15 +1,17 @@
+// Fetching data fromt the pokemon API
 async function fetchPokemonData(pokemonName) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
     const pokemonData = await response.json();
     return pokemonData;
 }
 
-document.addEventListener("input", async () => {
+// Creating an Event Listener to run the program when the input has changed in the search bar
+document.addEventListener("input", async () => { // "input" event sets to run code upon typing into the search bar, no need to submit
     let pokeName= document.getElementById('search-name').value.toLowerCase();
     const pokemonData = await fetchPokemonData(pokeName);
     const pokemonInfoElement = document.getElementById('pokemon-info');
     
-    
+    // Displays the pokemon data based on the value entered into the search bar
     pokemonInfoElement.innerHTML = `
     <div class="m-2">
         <h2>Name: ${pokemonData.name}</h2>
